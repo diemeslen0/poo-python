f93d0f75a525c7920fceb27c60598b0f5cd0c63e
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2018-03-04 21:07:20 -0300

    Adding comments to the class and methods

--- conta.py
+++ conta.py
@@ -1,7 +1,9 @@
 
 
 class Conta:
-    
+    '''
+    Class that represents a simple bank account
+    '''    
     def __init__(self, numero, titular, saldo, limite=1000.0):
         self.__numero  = numero
         self.__titular = titular
@@ -9,14 +11,26 @@ class Conta:
         self.__limite = limite
 
     def depositar(self, valor):
+        '''
+        Method to deposit a value in a bank account
+        '''
         self.__saldo += valor
     
     def sacar(self, valor):
+        '''
+        Method to remove a velue from a bank accountss
+        '''
         self.__saldo -= valor
     
     def transferir(self, valor, destino):
+        '''
+        Method to transfer a value between bank accounts
+        '''
         self.sacar(valor)
         destino.depositar(valor)
     
     def extrato(self):
+        '''
+        Method to get information from a bank account
+        '''
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
